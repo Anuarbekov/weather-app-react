1f978e77bfa65b6d3834886bd073eb5237fbec30
Meir <mikonek.a@gmail.com>
2022-06-15 00:33:52 +0600

    master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
-
+import $ from 'jquery';
+const APIKEY = "361ebb12802e23acedfab01998e0a8d0";
+const city = "New York";
 function App() {
-  
+  $('document').ready(
+    $.ajax({
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`,
+      contentType: "application/json",
+      dataType: 'json',
+      success: function(result){
+          console.log(result);
+      },
+      error: function(error){
+        console.log(error);
+      }
+  })
+  );
   return (
     <div className="App">
       
