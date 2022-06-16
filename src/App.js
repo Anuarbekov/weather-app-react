28a080e1c9549598dc2e472b6704439fbed892c8
Meir <mikonek.a@gmail.com>
2022-06-16 16:37:33 +0600

    messages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [error, setError] = useState();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState("End");
-
+  const [error, setError] = useState(false);
+  const [items, setItems] = useState("");
   const APIKEY = "361ebb12802e23acedfab01998e0a8d0";
   const city = "Almaty";
-
   const getWeather = () => {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`
@@ -15,19 +12,20 @@ function App() {
       .then((res) => res.json())
       .then(
         (result) => {
-          setIsLoaded(true);
           setItems(result);
         },
         (error) => {
-          setIsLoaded(false);
           setError(error);
         }
       );
   };
   return (
-    <div>
-      <button onClick={getWeather()}>Know the weather</button>
-      <h3>5</h3>
+    <div className="main">
+      <button onClick={getWeather} className="btn btn-success">
+        Weather in Almaty
+      </button>
+      <h3>{items === "" ? items : `Current temperature:  ${items.main.temp} ℃`}</h3>
+      <h3>{error === false ? "" : error}</h3>
     </div>
   );
 }
