a9ec59ab054d9a85ae690ffae6e74cbf56dfd718
Meir <mikonek.a@gmail.com>
2022-06-16 14:18:47 +0600

    master

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 function App() {
-  const [error, setError] = useState(null);
+  const [error, setError] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState("End");
 
   const APIKEY = "361ebb12802e23acedfab01998e0a8d0";
   const city = "Almaty";
-  // Примечание: пустой массив зависимостей [] означает, что
-  // этот useEffect будет запущен один раз
-  // аналогично componentDidMount()
-  useEffect(() => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
+
+  const getWeather = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`
+    )
       .then((res) => res.json())
       .then(
         (result) => {
           setIsLoaded(true);
           setItems(result);
         },
-        // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
-        // чтобы не перехватывать исключения из ошибок в самих компонентах.
         (error) => {
-          setIsLoaded(true);
+          setIsLoaded(false);
           setError(error);
         }
       );
-  }, []);
-  if (error) {
-    return <div>Ошибка: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Загрузка...</div>;
-  } else {
-    return (
-      <div>{items.main.temp}</div>
-    );
-  }
+  };
+  return (
+    <div>
+      <button onClick={getWeather()}>Know the weather</button>
+      <h3>5</h3>
+    </div>
+  );
 }
 export default App;
