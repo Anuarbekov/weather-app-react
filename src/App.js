b4b7aea2246c0797ce54156227eab19f1df59b0c
Meir <mikonek.a@gmail.com>
2022-06-16 13:17:38 +0600

    master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import './App.css';
-import $ from 'jquery';
-const APIKEY = "361ebb12802e23acedfab01998e0a8d0";
-const city = "New York";
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
-  $(document).ready(
-    $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=361ebb12802e23acedfab01998e0a8d0",
-      contentType: "application/json",
-      dataType: 'json',
-      success: function(result){
-          console.log(result);
-      },
-      error: function(error){
-        console.log(error);
-      }
-  })
-  );
-  return (
-    <div className="App">
-      <h1>Text</h1>
-    </div>
-  );
-}
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
+  const APIKEY = "361ebb12802e23acedfab01998e0a8d0";
+  const city = "Almaty";
+  // Примечание: пустой массив зависимостей [] означает, что
+  // этот useEffect будет запущен один раз
+  // аналогично componentDidMount()
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+        // чтобы не перехватывать исключения из ошибок в самих компонентах.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return <div>Ошибка: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Загрузка...</div>;
+  } else {
+    return (
+      <div>{items.main.temp}</div>
+    );
+  }
+}
 export default App;
